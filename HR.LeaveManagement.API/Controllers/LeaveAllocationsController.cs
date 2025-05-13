0f3cc19f435e991279884a2b01f2cceb77b3166f
Booklynn <95514331+Booklynn@users.noreply.github.com>
2025-05-13 22:19:51 +0700

    Init LeaveAllocation controller

---
+++ HR.LeaveManagement.API/Controllers/LeaveAllocationsController.cs
@@ -0,0 +1,44 @@
+﻿using HR.LeaveManagement.Application.Common;
+using HR.LeaveManagement.Application.Features.LeaveAllocation.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace HR.LeaveManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeaveAllocationsController(IDispatcher dispatcher) : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IReadOnlyList<LeaveAllocationDTO> Get()
+        {
+            return null;
+        }
+
+        // GET api/<LeaveAllocationsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<LeaveAllocationsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<LeaveAllocationsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<LeaveAllocationsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
