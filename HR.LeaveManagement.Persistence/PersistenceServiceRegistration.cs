a4d2c55f0b262ce844846128e358edea5b2425dd
Booklynn <95514331+Booklynn@users.noreply.github.com>
2025-05-17 21:07:42 +0700

    Add support multiple db providers

--- HR.LeaveManagement.Persistence/PersistenceServiceRegistration.cs
+++ HR.LeaveManagement.Persistence/PersistenceServiceRegistration.cs
@@ -11,11 +11,30 @@ public static class PersistenceServiceRegistration
 {
     public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
     {
+        var provider = configuration.GetValue<string>("DbProvider");
+
         services.AddDbContext<HrDatabaseContext>(options =>
         {
-            options.UseSqlServer(configuration.GetConnectionString("HrDatabaseConnectionString"));
-        });
+            switch (provider?.ToLowerInvariant())
+            {
+                case "sqlserver":
+                    options.UseSqlServer(configuration.GetConnectionString("HrDatabaseConnectionString"));
+                    break;
 
+                /*case "sqlite":
+                    options.UseSqlite(config.GetConnectionString("HrDatabaseSqliteConnectionString"));
+                    break;
+
+                case "inmemory":
+                    options.UseInMemoryDatabase("TestDb");
+                    break;*/
+
+                default:
+                    throw new InvalidOperationException($"Unsupported provider: {provider}");
+            }
+            ;
+        });
+       
         services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
         services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
         services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
