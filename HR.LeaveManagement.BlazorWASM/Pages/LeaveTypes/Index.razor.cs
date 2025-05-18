ede944b80913805844f75b930b4ce7dcda937eed
Booklynn <95514331+Booklynn@users.noreply.github.com>
2025-05-19 00:36:14 +0700

    Update Blazor

---
+++ HR.LeaveManagement.BlazorWASM/Pages/LeaveTypes/Index.razor.cs
@@ -0,0 +1,58 @@
+using HR.LeaveManagement.BlazorWASM.Contracts;
+using HR.LeaveManagement.BlazorWASM.Models.LeaveTypes;
+using Microsoft.AspNetCore.Components;
+
+namespace HR.LeaveManagement.BlazorWASM.Pages.LeaveTypes
+{
+    public partial class Index
+    {
+        [Inject]
+        public required NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        public required ILeaveTypeService LeaveTypeService { get; set; }
+
+        public IReadOnlyList<LeaveTypeViewModel> LeaveTypes { get; private set; } = [];
+
+        public string Message { get; set; } = string.Empty;
+
+        protected override async Task OnInitializedAsync()
+        {
+            LeaveTypes = await LeaveTypeService.GetLeaveTypes();
+        }
+
+        protected void CreateLeaveType()
+        {
+            NavigationManager.NavigateTo("/leavetypes/create");
+        }
+
+        protected void AllocationLeaveType(int id)
+        {
+            
+        }
+
+        protected void EditLeaveType(int id)
+        {
+            NavigationManager.NavigateTo($"/leavetypes/edit/{id}");
+        }
+
+        protected void DetailsLeaveType(int id)
+        {
+            NavigationManager.NavigateTo($"/leavetypes/details/{id}");
+        }
+
+        protected async Task DeleteLeaveTypeAsync(int id)
+        {
+            var response = await LeaveTypeService.DeleteLeaveType(id);
+            if (response.Success)
+            {
+                LeaveTypes = await LeaveTypeService.GetLeaveTypes();
+                StateHasChanged();
+            }
+            else
+            {
+                Message = response.Message;
+            }
+        }
+    }
+}
--- HR.LeaveManagement.BlazorWASM/Services/Base/Response.cs
+++ HR.LeaveManagement.BlazorWASM/Services/Base/Response.cs
@@ -2,7 +2,7 @@
 
 public class Response<T>
 {
-    public string? Message { get; set; }
+    public string Message { get; set; } = string.Empty;
     public string? ValidatioErrors { get; set; }
     public bool Success { get; set; }
     public T? Data { get; set; }
